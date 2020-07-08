2252d050deea4695d86b9d4ddb93cb9b75e04dbc
Mh-Tahir <58627780+Mh-Tahir@users.noreply.github.com>
2020-07-08 22:49:08 +0300

    Update index.js
    added search on page

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
-import './style.css';
+var input, search, pr, result, result_arr, locale_HTML, result_store;
 
-const appDiv = document.getElementById('app');
+function func() {
+  locale_HTML = document.body.innerHTML;   // сохраняем в переменную весь body (Первоначальный)
+}
+setTimeout(func, 1000);  //ждем подгрузки Jsona и выполняем
+
+function FindOnPage(name, status) {
+
+  input = document.getElementById(name).value; //получаем значение из поля в html
+
+
+  function FindOnPageGo() {
+
+    search = '/' + input + '/g';  //делаем из строки регуярное выражение
+    pr = document.body.innerHTML;   // сохраняем в переменную весь body
+    result = pr.match(/>(.*?)</g);  //отсекаем все теги и получаем только текст
+    result_arr = [];   //в этом массиве будем хранить результат работы (подсветку)
+
+    var warning = true;
+    for (let i = 0; i < result.length; i++) {
+      if (result[i].match(eval(search)) != null) {
+        warning = false;
+      }
+    }
+    if (warning == true) {
+      alert('No matches found');
+    }
+
+    for (let i = 0; i < result.length; i++) {
+      result_arr[i] = result[i].replace(eval(search), '<span style="background-color:yellow;">' + input + '</span>'); //находим нужные элементы, задаем стиль и сохраняем в новый массив
+    }
+    for (let i = 0; i < result.length; i++) {
+      pr = pr.replace(result[i], result_arr[i])  //заменяем в переменной с html текст на новый из новогом ассива
+    }
+    document.body.innerHTML = pr;  //заменяем html код
+  }
+
+  function FindOnPageBack() { document.body.innerHTML = locale_HTML; }
+
+  if (status) { FindOnPageBack(); FindOnPageGo(); } //чистим прошлое и Выделяем найденное
+  if (!status) { FindOnPageBack(); } //Снимаем выделение
+}
